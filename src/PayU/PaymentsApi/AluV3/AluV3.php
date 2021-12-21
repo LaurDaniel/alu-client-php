44f138eae2fb86c552d38cdaeffb457423426181
LaurDaniel <67411075+LaurDaniel@users.noreply.github.com>
2021-12-21 21:51:57 +0200

    Update AluV3.php

--- src/PayU/PaymentsApi/AluV3/AluV3.php
+++ src/PayU/PaymentsApi/AluV3/AluV3.php
@@ -31,7 +31,7 @@ final class AluV3 implements AuthorizationPaymentsApiClient
         'ro' => 'https://secure.payu.ro',
         'ru' => 'http://secure.payu.ru',
         'tr' => 'https://secure.payu.com.tr',
-        'sandbox' => 'https://sandbox.payu.com.ro',
+        'sandbox' => 'https://sandbox.payu.ro',
     ];
 
     /**
